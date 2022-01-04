75244fa7fc20cb3397e6b1df22e5a5e5ceb24bd4
Priyanka_sati <priyankasati1718@gmail.com>
2022-01-04 21:46:37 +0530

    first commit

---
+++ src/requests.js
@@ -0,0 +1,15 @@
+const API_KEY = "e522f23e43232c5446733384cce94aec";
+
+// /trending/all/week?api_key=e522f23e43232c5446733384cce94aec&language=en-US
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovie: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovie: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
